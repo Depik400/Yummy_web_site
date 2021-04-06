175a886ea58498a2facc4c81ec56333ffab0489b
Pavel <pro100cuber@gmail.com>
2021-04-06 18:09:49 +0700

    modified:   controllers/user_controller.js 	modified:   public/style/style.css

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -30,28 +30,30 @@ exports.logout = function (req, res) {
   });
 };
 
-exports.profile = (req, res) => { 
+exports.profile = (req, res) => {
   console.log(req.user.role);
-  if(req.user.role == "administrator"){
-    res.redirect('/user/admin')
-  }
-  res.send("successful login");
+  if (req.user.role == "administrator") {
+    res.redirect("/user/admin");
+  } else res.send("successful login");
 };
 
 exports.profile_admin = function (req, res) {
   if (req.user != undefined) {
     if (req.user.role == "administrator") {
       console.log(req.user.role);
-      user.find({}).lean().exec((err, user) => {
-        var json = JSON.stringify(user);
-       // console.log(json);
-        res.render("index", {
-          cases: "administrator",
-          username: req.user.username,
-          NotLogin: false,
-          users: user
+      user
+        .find({})
+        .lean()
+        .exec((err, user) => {
+          var json = JSON.stringify(user);
+          // console.log(json);
+          res.render("index", {
+            cases: "administrator",
+            username: req.user.username,
+            NotLogin: false,
+            users: user,
+          });
         });
-      });
     }
   }
 };
